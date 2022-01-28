9d42bab09e25a49e098470bc70b3cfebd8c22a2a
machao07 <machao_07@163.com>
2022-01-28 11:20:42 +0800

    edit

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,18 +2,10 @@
 module.exports = {
     printWidth: 100,
     tabWidth: 4,
-    useTabs: false,
     semi: false, // 未尾逗号
     singleQuote: true, // 单引号
-    quoteProps: 'as-needed',
     bracketSpacing: true,
     trailingComma: 'none', // 未尾分号
-    jsxBracketSameLine: false,
-    jsxSingleQuote: false,
-    arrowParens: 'always',
-    insertPragma: false,
-    requirePragma: false,
     proseWrap: 'never',
-    htmlWhitespaceSensitivity: 'strict',
-    endOfLine: 'lf'
+    htmlWhitespaceSensitivity: 'strict'
 };
