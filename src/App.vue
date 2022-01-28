773d3a2a16e3d42230cf55ae3e859e6c6529d2aa
machao07 <machao_07@163.com>
2022-01-28 10:18:03 +0800

    beauty

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-    <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
     <div id="app">
         <router-view />
     </div>
 </template>
 
 <style lang="scss">
-@import '../src/styles/reset.css';
+@import '@/src/styles/reset.css';
 </style>
